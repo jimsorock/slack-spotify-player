8f05328a07d010a7847588bd837d4318c1550378
Jimmy Muga <jmuga@crowdrise.com>
2017-06-26 15:49:37 -0700

    http get

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express()
 require('dotenv').config()
 const port = process.env.PORT || 8080
 const SpotifyWebApi = require('spotify-web-api-node')
+const http = require('http')
 
 const spotifyApi = new SpotifyWebApi({
     clientId: process.env.CLIENT_ID || 'foo',
@@ -49,7 +50,7 @@ app.post('/', (req, res) => {
     console.log('access token:', spotifyApi.getAccessToken())
     if(!spotifyApi.getAccessToken()) {
         const authorizeURL = spotifyApi.createAuthorizeURL(scopes, state)
-        res.redirect(307, authorizeURL)
+        http.get(authorizeURL)
     }
     spotifyApi.getMyCurrentPlaybackState()
         .then(({body: {item: track}}) => {
